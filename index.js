d86595c3706573abdd09cb59ca936662ea23c38c
bre-nda <brenda.kanale@student.moringaschool.com>
2021-08-01 23:50:41 +0300

    add changes to the file

--- index.js
+++ index.js
@@ -23,3 +23,21 @@ var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
                 location.reload();
                 alert("Please select a pizza size");
         };
+        var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
+                switch (pizzaSize) {
+                    case "":
+                        priceSize = 0;
+                        break;
+                    case "large":
+                        priceSize = 1200;
+                        break;
+                    case "medium":
+                        priceSize = 800;
+                        break;
+                    case "small":
+                        priceSize = 650;
+                        break;
+                    default:
+                        location.reload();
+                        alert("Please select a pizza size");
+                };
