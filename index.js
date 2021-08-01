cc683b35a62d71021b81bdffdf5f9c89a1b04855
bre-nda <brenda.kanale@student.moringaschool.com>
2021-08-01 23:59:09 +0300

    add if condition

--- index.js
+++ index.js
@@ -6,37 +6,48 @@ function Pizza(pizza, size, crust, toppings) {
 }
 var priceSize, priceCrust, priceTopping;
 var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
-        switch (pizzaSize) {
-            case "":
-                priceSize = 0;
-                break;
-            case "large":
-                priceSize = 1200;
-                break;
-            case "medium":
-                priceSize = 800;
-                break;
-            case "small":
-                priceSize = 650;
-                break;
-            default:
-                location.reload();
-                alert("Please select a pizza size");
-        };
-        switch (pizzaCrust) {
-            case "":
-                priceCrust = 0;
-                break;
-            case "crispy":
-                priceCrust = 100;
-                break;
-            case "stuffed":
-                priceCrust = 200;
-                break;
-            case "thick":
-                priceCrust = 150;
-                break;
-            default:
-                location.reload();
-                alert("Please choose a crust");
-        };
+    switch (pizzaSize) {
+        case "":
+            priceSize = 0;
+            break;
+        case "large":
+            priceSize = 1200;
+            break;
+        case "medium":
+            priceSize = 800;
+            break;
+        case "small":
+            priceSize = 650;
+            break;
+        default:
+            location.reload();
+            alert("Please select a pizza size");
+    };
+    switch (pizzaCrust) {
+        case "":
+            priceCrust = 0;
+            break;
+        case "crispy":
+            priceCrust = 120;
+            break;
+        case "stuffed":
+            priceCrust = 200;
+            break;
+        case "thick":
+            priceCrust = 150;
+            break;
+        default:
+            location.reload();
+            alert("Please choose a crust");
+    };
+    if (pizzaSize == 'large') {
+        priceTopping = pizzaTopping.length * 200;
+    } else if (pizzaSize == 'medium') {
+        priceTopping = pizzaTopping.length * 150;
+    } else if (pizzaSize == 'small') {
+        priceTopping = pizzaTopping.length * 100;
+    }
+
+    var pizzaTotal = priceSize + priceCrust + priceTopping;
+    return pizzaTotal;
+}
