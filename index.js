3332e96ea2df6707b6fced8cf96cc988eb9c779e
bre-nda <brenda.kanale@student.moringaschool.com>
2021-08-01 23:56:26 +0300

    add chnages to the files

--- index.js
+++ index.js
@@ -11,33 +11,32 @@ var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
                 priceSize = 0;
                 break;
             case "large":
-                priceSize = 1000;
+                priceSize = 1200;
                 break;
             case "medium":
                 priceSize = 800;
                 break;
             case "small":
-                priceSize = 550;
+                priceSize = 650;
                 break;
             default:
                 location.reload();
                 alert("Please select a pizza size");
         };
-        var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
-                switch (pizzaSize) {
-                    case "":
-                        priceSize = 0;
-                        break;
-                    case "large":
-                        priceSize = 1200;
-                        break;
-                    case "medium":
-                        priceSize = 800;
-                        break;
-                    case "small":
-                        priceSize = 650;
-                        break;
-                    default:
-                        location.reload();
-                        alert("Please select a pizza size");
-                };
+        switch (pizzaCrust) {
+            case "":
+                priceCrust = 0;
+                break;
+            case "crispy":
+                priceCrust = 100;
+                break;
+            case "stuffed":
+                priceCrust = 200;
+                break;
+            case "thick":
+                priceCrust = 150;
+                break;
+            default:
+                location.reload();
+                alert("Please choose a crust");
+        };
