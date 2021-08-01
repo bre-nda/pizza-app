020ca17a810641899850c1c70ef021aae89a347c
bre-nda <brenda.kanale@student.moringaschool.com>
2021-08-02 00:08:00 +0300

    add content to the file

--- index.js
+++ index.js
@@ -52,31 +52,61 @@ var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
     return pizzaTotal;
 }
 $(document).ready(function() {
-            $("#order-online").click(function() {
-                $(".view-two").show();
-                $(".view-one").hide();
-            })
-            $("#continue").click(function(event) {
-                        event.preventDefault();
-                        $(".view-three").show();
-                        $(".view-two").hide();
-                        //get form values
-                        let pizzaName = $("#pizza option:selected").val();
-                        let pizzaSize = $("#size option:selected").val();
-                        let pizzaCrust = $("#crust option:selected").val();
-                        var pizzaTopping = [];
-                        $("input:checkbox[name=toppings]:checked").each(function() {
-                            pizzaTopping.push($(this).val());
-                        });
-                        var total = price(pizzaSize, pizzaCrust, pizzaTopping);
-                        var grandTotal = total + 300;
-                        var order = new Pizza(pizzaName, pizzaSize, pizzaCrust, pizzaTopping)
-                        $(".current-order").append('<tr><td id="name">' + order.pizza + '</td><td id="size">' + order.size + '</td><td id="crust">' + order.crust + '</td><td id="toppings">' + order.toppings + '</td><td id="total">' + total);
+    $("#order-online").click(function() {
+        $(".view-two").show();
+        $(".view-one").hide();
+    })
+    $("#continue").click(function(event) {
+        event.preventDefault();
+        $(".view-three").show();
+        $(".view-two").hide();
+        //get form values
+        let pizzaName = $("#pizza option:selected").val();
+        let pizzaSize = $("#size option:selected").val();
+        let pizzaCrust = $("#crust option:selected").val();
+        var pizzaTopping = [];
+        $("input:checkbox[name=toppings]:checked").each(function() {
+            pizzaTopping.push($(this).val());
+        });
+        var total = price(pizzaSize, pizzaCrust, pizzaTopping);
+        var grandTotal = total + 300;
+        var order = new Pizza(pizzaName, pizzaSize, pizzaCrust, pizzaTopping)
+        $(".current-order").append('<tr><td id="name">' + order.pizza + '</td><td id="size">' + order.size + '</td><td id="crust">' + order.crust + '</td><td id="toppings">' + order.toppings + '</td><td id="total">' + total);
 
-                        //Pickup button
-                        $("#pick-up").click(function() {
-                            alert("Dear customer, your order will be ready for pickup in 1 hour. Your order total is: " + total);
+        //Pickup button
+        $("#pick-up").click(function() {
+            alert("Dear customer, your order will be ready for pickup in 1 hour. Your order total is: " + total);
 
-                            //refresh page
-                            location.reload();
-                        })
+            //refresh page
+            location.reload();
+        })
+        $("#checkout").click(function() {
+            //form data
+            var clientName = $("#full-name").val();
+            var clientNumber = $("#phone-number").val();
+            var clientLocation = $("#location").val();
+
+            if (clientName === "" || clientNumber === "" || clientLocation === "") {
+                alert("Please fill in the delivery form. All fields are required")
+            } else {
+                alert("Dear " + clientName + " your order will be delivered to " + clientLocation + " within the hour! Your order total is: " +
+                    grandTotal + " Our rider will call you on arrival");
+            }
+        })
+    })
+    $("#add").click(function(event) {
+        event.preventDefault();
+
+        //Add an extra order
+        $(".view-three").hide();
+        $(".view-four").hide();
+        $(".view-two").show();
+        document.getElementById("form1").reset();
+    })
+
+    //Delivery button
+    $("#delivery").click(function() {
+        $(".table-buttons").hide();
+        $(".view-four").slideDown();
+    })
+})
