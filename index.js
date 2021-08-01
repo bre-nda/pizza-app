e2d5ca85b5b15a926dd8e3045c82e1df66e854c3
bre-nda <brenda.kanale@student.moringaschool.com>
2021-08-01 23:47:16 +0300

    add switch case

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function Pizza(pizza, size, crust, toppings) {
     this.toppings = toppings;
 }
 var priceSize, priceCrust, priceTopping;
+var price = function(pizzaSize, pizzaCrust, pizzaTopping) {
+        switch (pizzaSize) {
+            case "":
+                priceSize = 0;
+                break;
+            case "large":
+                priceSize = 1000;
+                break;
+            case "medium":
+                priceSize = 800;
+                break;
+            case "small":
+                priceSize = 550;
+                break;
+            default:
+                location.reload();
+                alert("Please select a pizza size");
+        };
